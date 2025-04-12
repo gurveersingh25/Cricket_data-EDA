71d802588dcc1930757fde91136841601c364598
Gurveer Singh <150374263+gurveersingh25@users.noreply.github.com>
2025-04-12 19:13:06 +0530

    Create EDA.py

---
+++ EDA.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_excel(r"C:\Users\india\OneDrive\Documents\Cricket_data.xlsx", sheet_name="Cricket_data")
+df.head()
